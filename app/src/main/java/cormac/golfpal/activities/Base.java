46f2b15058cff250b7f5e2210c93209e523a6cba
cormac-dunphy <cormac-d@hotmail.com>
2019-02-18 15:27:35 +0000

    implemented adding courses to sqlite database and reading from the database and displaying to home screen

--- app/src/main/java/cormac/golfpal/activities/Base.java
+++ app/src/main/java/cormac/golfpal/activities/Base.java
@@ -22,6 +22,7 @@ public class Base extends AppCompatActivity {
     public static ArrayList<Course> favouriteList = new ArrayList<>();
     public static String beforeUpdateName;
     public int beforeUpdatePosition;
+    ArrayList<Course> dbCourseList = new ArrayList<>();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
