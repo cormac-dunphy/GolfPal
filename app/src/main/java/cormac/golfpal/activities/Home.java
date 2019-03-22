c108e4aea32a0f626fbf5bae1dcb1c26a0a5db52
cormac-dunphy <cormac-d@hotmail.com>
2019-03-22 13:46:29 +0000

    added basic nav drawer

--- app/src/main/java/cormac/golfpal/activities/Home.java
+++ app/src/main/java/cormac/golfpal/activities/Home.java
@@ -1,17 +1,25 @@
 package cormac.golfpal.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenuPresenter;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -25,6 +33,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +46,8 @@ import cormac.golfpal.utils.CourseListViewAdapter;
 import cormac.golfpal.utils.FavListViewAdapter;
 
 import static android.widget.Toast.LENGTH_SHORT;
+import static cormac.golfpal.R.id.navImage;
+import static cormac.golfpal.R.id.navNumberOfCourses;
 
 public class Home extends Base {
     TextView emptyList;
@@ -43,6 +55,10 @@ public class Home extends Base {
     DatabaseReference databaseCourses;
     List<Course> courseList;
     List<Course> favList;
+    private DrawerLayout drawerLayout;
+    int count;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +66,29 @@ public class Home extends Base {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        drawerLayout = findViewById(R.id.drawer_layout);
         FirebaseApp.initializeApp(this);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        Uri photourl = user.getPhotoUrl();
-        Log.i("photourl", "onCreate: " + String.valueOf(photourl));
+        final Uri photoUrl = user.getPhotoUrl();
+        final String userName = user.getDisplayName();
+        count = 0;
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(Home.this, SignIn.class));
+                }
+            }
+        };
+
+        NavigationView nav_view = findViewById(R.id.nav_view);
+        nav_view.setItemIconTintList(null);
+        Log.i("photourl", "onCreate: " + String.valueOf(photoUrl));
         Log.i("googleSignIn", "onCreate: user = " + user);
         if(user == null){
             Intent toSignIn = new Intent(Home.this, SignIn.class);
@@ -94,6 +129,34 @@ public class Home extends Base {
                 return true;
             }
         });
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                //use picasso library to cast users google picture to the nav drawer
+                Picasso.with(getApplicationContext()).load(photoUrl).into((ImageView) findViewById(navImage));
+                //set Text View to user name
+                TextView name = findViewById(R.id.navUsername);
+                name.setText(userName);
+                TextView numberOfCourses = findViewById(R.id.navNumberOfCourses);
+                numberOfCourses.setText(String.valueOf(count) + " courses");
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
     }
 
     private boolean checkUser() {
@@ -110,6 +173,9 @@ public class Home extends Base {
     @Override
     protected void onStart() {
         super.onStart();
+        count = 0;
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.addAuthStateListener(mAuthListener);
         if (checkUser() == false) {
             Intent toSignIn = new Intent(Home.this, SignIn.class);
             startActivity(toSignIn);
@@ -121,6 +187,7 @@ public class Home extends Base {
                     for (DataSnapshot courseSnapShot : dataSnapshot.getChildren()) {
                         Course course = courseSnapShot.getValue(Course.class);
                         courseList.add(course);
+                        count++;
                     }
                     CourseListViewAdapter courseListViewAdapter = new CourseListViewAdapter(Home.this, courseList);
                     courseListView.setAdapter(courseListViewAdapter);
@@ -177,6 +244,12 @@ public class Home extends Base {
             return true;
         }
 
+        switch(item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
     //goes to add course page
@@ -212,4 +285,11 @@ public class Home extends Base {
         });
 
     }
+
+    public void menuLogOut(View view) {
+
+        mAuth.signOut();
+
+    }
+
 }
